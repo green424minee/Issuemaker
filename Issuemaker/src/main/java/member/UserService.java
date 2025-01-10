d6cf688e629e9c3e1060e20b425748f322fe85c2
000vin000 <q020112@naver.com>
2025-01-10 17:47:46 +0900

    user 회원가입

---
+++ Issuemaker/src/main/java/member/UserService.java
@@ -0,0 +1,33 @@
+package member;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import util.DBUtil;
+
+public class UserService {
+	private static final UserService instance = new UserService();
+	
+	public static UserService getInstance() {
+		return instance;
+	}
+	
+	private UserService() {}
+	
+	public int insert (User user) {
+		try (SqlSession session = DBUtil.getSqlSession()) {
+			UserMapper mapper = session.getMapper(UserMapper.class);
+			
+			int row = mapper.insert(user);
+		
+			
+			session.commit();
+				
+			return row;
+		}
+	}
+	
+
+}
+
