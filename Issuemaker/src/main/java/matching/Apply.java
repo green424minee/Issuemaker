38099bf3d85a6f771095ef5738e457dcc07c4fb8
mingmingDD <minji970@naver.com>
2025-01-10 09:43:30 +0900

    클래스 생성자, Getter/Setter, toString 생성

--- Issuemaker/src/main/java/matching/Apply.java
+++ Issuemaker/src/main/java/matching/Apply.java
@@ -1,5 +1,56 @@
 package matching;
 
-public class Apply {
+import myPage.Resume;
 
+public class Apply {
+	private Notice notice;
+	private Resume resume;
+	private int view;
+	private int result;
+	
+	public Apply(Notice notice, Resume resume, int view, int result) {
+		super();
+		this.notice = notice;
+		this.resume = resume;
+		this.view = view;
+		this.result = result;
+	}
+
+	public Notice getNotice() {
+		return notice;
+	}
+
+	public void setNotice(Notice notice) {
+		this.notice = notice;
+	}
+
+	public Resume getResume() {
+		return resume;
+	}
+
+	public void setResume(Resume resume) {
+		this.resume = resume;
+	}
+
+	public int getView() {
+		return view;
+	}
+
+	public void setView(int view) {
+		this.view = view;
+	}
+
+	public int getResult() {
+		return result;
+	}
+
+	public void setResult(int result) {
+		this.result = result;
+	}
+
+	@Override
+	public String toString() {
+		return "Apply [notice=" + notice + ", resume=" + resume + ", view=" + view + ", result=" + result + "]";
+	}
+	
 }
