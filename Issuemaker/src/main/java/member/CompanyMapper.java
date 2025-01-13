6a5e5e605a4c860b5b431739926b2e465213a32c
000vin000 <00vin0vin0vin00@naver.com>
2025-01-13 10:13:14 +0900

    민지코드 수정

--- Issuemaker/src/main/java/member/CompanyMapper.java
+++ Issuemaker/src/main/java/member/CompanyMapper.java
@@ -5,7 +5,7 @@ import org.apache.ibatis.annotations.Insert;
 public interface CompanyMapper {
 	 
 	 @Insert("INSERT INTO company (comId, comName, comNo, comPhone, comCeo, managerEmail, comAddress, comBirth, comSize, comWeb) " +
-	            "VALUES (#{comid}, #{comName}, #{comNo}, #{comPhone}, #{comCeo}, #{managerEmail}, #{comAddress}, #{comBirth}, #{comSize}, #{comWeb})")
+	            "VALUES (#{comId}, #{comName}, #{comNo}, #{comPhone}, #{comCeo}, #{managerEmail}, #{comAddress}, #{comBirth}, #{comSize}, #{comWeb})")
 	 int insert(Company company);
 
 
